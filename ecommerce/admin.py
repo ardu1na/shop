58d833e823f404332981b34fb980204fc2da3049
ardu1na <arduinadelbosque@gmail.com>
2023-08-03 12:28:51 -0300

    modeling

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecommerce.models import Product, ProductCart, Cart, Client, Category, Subcategory, Location, PayMethod
+
+
+admin.site.register(Product)
+admin.site.register(ProductCart)
+admin.site.register(Cart)
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Location)
+admin.site.register(PayMethod)
+admin.site.register(Client)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,33 +1,60 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
+class ModelBase(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(auto_now=True, editable=False)
+    date_deleted = models.DateTimeField(editable=False, null=True)
+    state = models.BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        if self.state == False:
+            self.date_deleted = timezone.now() 
+        super().save(*args, **kwargs)
+    
+    class Meta:
+        abstract = True
+        
+    
+    
 
-class Category(models.Model):
+class Category(ModelBase):
     name = models.CharField(max_length=200)
 
-
-class Subcategory(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
+class Subcategory(ModelBase):
     name = models.CharField(max_length=200)
-
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+    def __str__(self):
+            return self.name
+        
 
-
-class Product(models.Model):
+class Product(ModelBase):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=3000)
     
     price = models.IntegerField()
+    
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='products')
 
+    def __str__(self):
+        return self.name
+    
 
 
-
-
-
-class Client(models.Model):
+class Client(ModelBase):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client')
+    def __str__(self):
+        return self.user.get_full_name()
+    
+
+
 
 class Location(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='locations')
@@ -38,16 +65,19 @@ class PayMethod(models.Model):
 
 
 
-
-
-
-class Cart(models.Model):
+class Cart(ModelBase):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='carts')
     total = models.PositiveIntegerField(default=0)
 
 
+    def __str__(self):
+        date_time = self.date_created.strftime('%H:%M %d/%m')
+        return f'{self.client} Cart - {date_time}'
+                 
+    
 
-class ProductCart(models.Model):
+
+class ProductCart(ModelBase):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='products')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='carts')
     ammount = models.PositiveSmallIntegerField(default=1)
