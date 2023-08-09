3caa5c0eb05e0d55482032a2acc7e0301019a78d
ardu1na <arduinadelbosque@gmail.com>
2023-08-09 15:48:36 -0300

    logout and testing endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path
 
 from users import views
+from users.views import LogoutView
 
 urlpatterns = [
     re_path('signup', views.signup),
     re_path('login', views.login),
-    re_path('logout', views.logout),
+    re_path('logout', LogoutView.as_view(), name="logout"),
     re_path('test_token', views.test_token),
 
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -15,13 +16,20 @@ from users.serializers import UserSerializer
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(username=request.data['username'])
+        username = serializer.validated_data['username']
+        
+        # Check if a user with the given username already exists
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already taken'}, status=status.HTTP_409_CONFLICT)  # HTTP 409 Conflict
+        
+        user = serializer.save()
         user.set_password(request.data['password'])
         user.save()
+        
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
-    return Response(serializer.errors, status=status.HTTP_200_OK)
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
@@ -34,13 +42,15 @@ def login(request):
 
 
 
-@api_view(['POST'])
-def logout(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    token = get_object_or_404(Token, user=user)
-    token.delete()
-    
-    return Response("bye!")
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        token = get_object_or_404(Token, user=user)
+        token.delete()
+        return Response({'message': f'Goodbye, {user.username}!'}, status=status.HTTP_200_OK)
 
 
 
