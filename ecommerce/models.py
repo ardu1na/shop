1a6b9f8d872b22e3d7ea3b8da41d8a7c19fd7678
ardu1na <arduinadelbosque@gmail.com>
2023-08-10 13:15:35 -0300

    populating detail categorie data in html

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -14,6 +14,7 @@ class ModelBase(models.Model):
         
 class Category(ModelBase):
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=3000, null=True, blank=True)
 
     
     def __str__(self):
