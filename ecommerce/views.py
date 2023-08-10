b63ecd3d2b628beb93271514c3023846289b3425
ardu1na <arduinadelbosque@gmail.com>
2023-08-10 14:29:04 -0300

    added all products into categories template

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -10,8 +10,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from ecommerce.models import Category
-from ecommerce.serializers import CategorySerializer, CategoryDetailSerializer
+from ecommerce.models import Category, Product
+from ecommerce.serializers import CategorySerializer, ProductSerializer, CategoryDetailSerializer
 
 
 @api_view(['GET'])
@@ -22,6 +22,15 @@ def category_detail(request, category_id):
         return Response(serializer.data)
     except Category.DoesNotExist:
         return Response(status=404)
+
+
+
+@api_view(['GET'])
+def products(request):
+    products = Product.objects.all()  # Retrieve all categories from the database
+    serializer = ProductSerializer(products, many=True)  # Serialize the queryset
+
+    return Response(serializer.data)  # Return serialized data in the response
 
 
 
