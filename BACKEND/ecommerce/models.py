a25523a0641bfe8760d3071b6299ac6cb58a9367
ardu1na <arduinadelbosque@gmail.com>
2023-10-05 18:37:43 -0300

    okidoky

--- BACKEND/ecommerce/models.py
+++ BACKEND/ecommerce/models.py
@@ -102,7 +102,6 @@ class Cart(ModelBase):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='carts', verbose_name="cliente")
     total = models.PositiveIntegerField(default=0, null=True, blank=True)
     done = models.BooleanField(default=False, verbose_name="cerrado")
-    products_q = models.SmallIntegerField(default=0, null=True, blank=True, verbose_name="cantidad")
     
     def __str__(self):
         date_time = self.date_created.strftime('%H:%M %d/%m')
